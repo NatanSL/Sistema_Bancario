37ae024493ade4299d1e0e15fcdae3d40b023750
NatanSL <ntsilvalima@gmail.com>
2024-09-10 14:26:59 -0300

    Implementação do código

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ menu = """
 
  """
 
-saldo = 0
+saldo = 30
 limite = 500
 extrato = ""
 numero_saques = 0
@@ -17,13 +17,40 @@ while True:
 
     opcao = input(menu)
     if opcao == "1":
-        print("Depósito")
+        valor_deposito = float(input("Qual o valor do depósito?"))
+        if valor_deposito > 0:
+            saldo += valor_deposito
+            extrato += f"Depósito: R$ {valor_deposito:.2f}\n"
+        else:
+            print ("Valor invalido, tente novamente!")       
 
     elif opcao == "2":
-        print("Saque")
+        valor_saque = float(input("Informe o valor do saque: "))
+        excedeu_saldo = valor_saque > saldo
+        excedeu_limite = valor_saque > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif excedeu_limite:
+            print ("Operação falhou! Voce excedeu o limite!")
+        elif excedeu_saques:
+            print ("Excedeu limite de saque!")
+
+        elif valor_saque > 0:
+            saldo -= valor_saque
+            extrato += f"Saque: R$ {valor_saque:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")                
 
     elif opcao == "3":
-        print ("Extrato")
+        
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
 
     elif opcao == "0":
         break
